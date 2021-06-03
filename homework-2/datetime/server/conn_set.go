a5618dd952d12e0c1815b5160e48de60c87f0713
eol morikvendy <emorikvendy@gmail.com>
2021-06-03 22:30:50 +0300

    1. Доработка приложения рассылки даты/времени

---
+++ homework-2/datetime/server/conn_set.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"net"
+	"sync"
+)
+
+type ConnSet struct {
+	mx sync.RWMutex
+	m  map[net.Conn]struct{}
+}
+
+func NewConnSet() *ConnSet {
+	return &ConnSet{
+		m: map[net.Conn]struct{}{},
+	}
+}
+func (set *ConnSet) Get(key net.Conn) (struct{}, bool) {
+	set.mx.RLock()
+	defer set.mx.RUnlock()
+	val, ok := set.m[key]
+	return val, ok
+}
+
+func (set *ConnSet) Has(key net.Conn) bool {
+	set.mx.RLock()
+	defer set.mx.RUnlock()
+	_, ok := set.m[key]
+	return ok
+}
+
+func (set *ConnSet) Add(key net.Conn, value struct{}) {
+	set.mx.Lock()
+	defer set.mx.Unlock()
+	set.m[key] = value
+}
+func (set *ConnSet) Delete(key net.Conn) {
+	set.mx.Lock()
+	defer set.mx.Unlock()
+	if _, ok := set.m[key]; ok {
+		delete(set.m, key)
+	}
+}
+
+func (set *ConnSet) Range(f func(key net.Conn)) {
+	set.mx.Lock()
+	defer set.mx.Unlock()
+	for conn := range set.m {
+		f(conn)
+	}
+}
--- homework-2/datetime/server/server.go
+++ homework-2/datetime/server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -9,6 +10,10 @@ import (
 	"sync"
 	"syscall"
 	"time"
+)
+
+var (
+	connections = NewConnSet()
 )
 
 func main() {
@@ -22,7 +27,8 @@ func main() {
 	defer close(cancel)
 	wg.Add(1)
 	go watchSignals(cancel, &wg)
-	go closeListener(listener, &wg)
+	go closeListener(listener, cancel, &wg)
+	go broadcaster(cancel)
 	for {
 		select {
 		case <-cancel:
@@ -35,13 +41,42 @@ func main() {
 				log.Printf("Error from connection: %v", err)
 				continue
 			}
+			connections.Add(conn, struct{}{})
 			wg.Add(1)
 			go handleConn(conn, cancel, &wg)
 		}
 	}
 }
+func broadcaster(cancel chan struct{}) {
+	for {
+		select {
+		case <-cancel:
+			cancel <- struct{}{}
+			log.Println("broadcaster stopped")
+			return
+		default:
+			var msg string
+			fmt.Scanln(&msg)
+			connections.Range(func(conn net.Conn) {
+				log.Printf("writing %s to %v", msg, conn)
+				io.WriteString(conn, msg)
+			})
+		}
+	}
+}
 
-func closeListener(listener net.Listener, wg *sync.WaitGroup) {
+func closeListener(listener net.Listener, cancel chan struct{}, wg *sync.WaitGroup) {
+	<-cancel
+	log.Println("closeListener started")
+	connections.Range(func(conn net.Conn) {
+		delete(connections.m, conn)
+		err := conn.Close()
+		if err != nil {
+			log.Printf("Error while closing connection: %v", err)
+		}
+	})
+	cancel <- struct{}{}
+	log.Print("All connections closed")
 	wg.Wait()
 	err := listener.Close()
 	if err != nil {
@@ -52,14 +87,16 @@ func closeListener(listener net.Listener, wg *sync.WaitGroup) {
 func watchSignals(cancel chan struct{}, wg *sync.WaitGroup) {
 	osSignalChan := make(chan os.Signal)
 	signal.Notify(osSignalChan, syscall.SIGINT, syscall.SIGTERM)
-	<-osSignalChan
+	sig := <-osSignalChan
+	log.Printf("got signal %+v", sig)
 	cancel <- struct{}{}
 	wg.Done()
 }
 
-func handleConn(c net.Conn, cancel chan struct{}, wg *sync.WaitGroup) {
+func handleConn(conn net.Conn, cancel chan struct{}, wg *sync.WaitGroup) {
 	defer func() {
-		err := c.Close()
+		connections.Delete(conn)
+		err := conn.Close()
 		if err != nil {
 			log.Printf("Error while closing connection: %v", err)
 		}
@@ -70,11 +107,11 @@ func handleConn(c net.Conn, cancel chan struct{}, wg *sync.WaitGroup) {
 	for {
 		select {
 		case <-cancel:
-			io.WriteString(c, "Server is stopping")
+			io.WriteString(conn, "Server is stopping")
 			cancel <- struct{}{}
 			return
 		case <-ticker.C:
-			_, err := io.WriteString(c, time.Now().Format("15:04:05"))
+			_, err := io.WriteString(conn, time.Now().Format("15:04:05"))
 			if err != nil {
 				return
 			}
